a27b77eaae9852d6cc4bc6d49a0e87ad68ce140d
iuill <57124181+iuill@users.noreply.github.com>
2022-07-16 10:47:01 +0900

    Add files via upload

---
+++ xml2tsv.ps1
@@ -0,0 +1,23 @@
+﻿
+Param(
+    [parameter(mandatory=$true)][string]$path
+)
+
+$xml = [XML](cat $path -Encoding UTF8)
+$nav = $xml.CreateNavigator()
+
+
+Write-Output "start`tend`tsource app`tvalue"
+
+$list = @()
+$format = "yyyy/MM/dd HH:mm:ss"
+$nav.Select("/HealthData/Record[@type='HKQuantityTypeIdentifierBodyMass']") | %{
+    $srcApp = $_.GetAttribute("sourceName", "")
+    $val = $_.GetAttribute("value", "")
+    $start = ([DateTime]($_.GetAttribute("startDate", ""))).ToString($format)
+    $end = ([DateTime]($_.GetAttribute("endDate", ""))).ToString($format)
+
+    $list += "$start`t$end`t$srcApp`t$val"
+}
+
+$list | sort | %{Write-Output $_}
